5fec4db9a9203cb781f374de49a34ff46d346988
HiiWee <wpdlzhf159@gmail.com>
2023-03-15 20:32:14 +0900

    feat: Jpa entityManager 생성 확인

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,11 +1,15 @@
 package hellojpa;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("hello");
-        System.out.println(factory);
+        EntityManager entityManager = factory.createEntityManager();
+
+        entityManager.close();
+        factory.close();
     }
 }
