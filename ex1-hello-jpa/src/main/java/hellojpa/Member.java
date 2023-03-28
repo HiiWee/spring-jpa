8aad9b65e7c300db77341d39d58e354302eec45f
HiiWee <wpdlzhf159@gmail.com>
2023-03-29 01:25:01 +0900

    style: 임포트문 정리

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,9 +1,7 @@
 package hellojpa;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
 public class Member {
